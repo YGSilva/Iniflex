be99263f0ced69610fbacb1a5b6848d5a345c0c0
Yago Silva <yago.mdk284@gmail.com>
2023-04-24 19:59:20 -0300

    adde deletemapping

--- src/main/java/com/project/iniflex/test/services/PersonService.java
+++ src/main/java/com/project/iniflex/test/services/PersonService.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.iniflex.test.dto.PersonDTO;
 import com.project.iniflex.test.entities.Person;
 import com.project.iniflex.test.repositories.PersonRepository;
+import com.project.iniflex.test.services.exceptions.DatabaseException;
 
 @Service
 public class PersonService {
@@ -23,5 +25,13 @@ public class PersonService {
 		
 		//Transforma a lista do tipo Person em PersonDTO
 		return list.stream().map(x -> new PersonDTO(x)).collect(Collectors.toList());		
+	}
+
+	public void delete(Long id) { 
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de Integridade");
+		}
 	}
 }
